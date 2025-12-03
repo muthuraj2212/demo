04d66264217de9ba08eca2e1b81c9d974d719b72
muthuraj2212 <muthuraj20044@gmail.com>
2025-12-03 18:15:44 +0530

    This is odd or even code for java

---
+++ demo.java
@@ -0,0 +1,11 @@
+
+class demo{
+    public static void main(String args[]){
+      int number=31;
+       if(number%2==0){ 
+        System.out.println("The number is even");
+    }else{
+        System.out.println("The number is odd");
+    }
+}
+}
